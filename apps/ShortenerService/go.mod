172b20bf26104caaf787bca80ae2e36e09f94ec0
vzsoares <vinicius.zenha@gmail.com>
2024-06-15 18:42:40 -0300

    feat: basic lambda config for ShortenerService
    i'm messing with vim fugitive, no idea what's happening

--- apps/ShortenerService/go.mod
+++ apps/ShortenerService/go.mod
@@ -1,3 +1,8 @@
 module apps/ShortenerService
 
 go 1.22
+
+require (
+	github.com/aws/aws-lambda-go v1.47.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
+)
--- apps/ShortenerService/main.go
+++ apps/ShortenerService/main.go
@@ -1,12 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+)
+
+type Response struct {
+	From    string `json:"from"`
+	Message string `json:"message"`
+}
+
+var httpLambda *httpadapter.HandlerAdapter
+
+func init() {
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+        json.NewEncoder(w).Encode(Response{From: "net/http", Message: time.Now().Format(time.UnixDate)+"123456"})
+	})
+
+	httpLambda = httpadapter.New(http.DefaultServeMux)
 }
 
 func main() {
-	fmt.Println(Hello("ShortenerService"))
+	lambda.Start(Handler)
+
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return httpLambda.ProxyWithContext(ctx, req)
 }
