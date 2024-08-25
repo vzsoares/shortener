d1901bfa795e6124fa73e0fee55e03f446962035
vzsoares <vinicius.zenha@gmail.com>
2024-08-25 12:59:16 -0300

    feat: dlt unused

--- apps/engine/store/url/main.go
+++ apps/engine/store/url/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"apps/engine/types"
 	"context"
-	"encoding/json"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
@@ -78,58 +76,4 @@ func (*DynamoStore) Put(ctx context.Context, item *types.Url) (bool, error) {
 
 func (*DynamoStore) Delete(ctx context.Context, rash string) error {
 	return nil
-}
-
-func Handler(ctx context.Context, event events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
-	cfg, err := config.LoadDefaultConfig(ctx)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	client := dynamodb.NewFromConfig(cfg, func(o *dynamodb.Options) {
-		o.EndpointOptions.DisableHTTPS = true
-		o.BaseEndpoint = aws.String(BaseEndpoint)
-	})
-
-	input := &dynamodb.GetItemInput{
-		TableName: &TableName,
-		Key: map[string]ddbtypes.AttributeValue{
-			"city": &ddbtypes.AttributeValueMemberS{Value: "Mumbai"},
-			"Name": &ddbtypes.AttributeValueMemberS{Value: "Ali"},
-		}}
-
-	res, err := client.GetItem(ctx, input)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	body, err := json.Marshal(res)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	qinput := &dynamodb.QueryInput{
-		TableName:              &TableName,
-		KeyConditionExpression: aws.String("#name = :name"),
-		ExpressionAttributeValues: map[string]ddbtypes.AttributeValue{
-			":name": &ddbtypes.AttributeValueMemberS{Value: "Ali"},
-		},
-		ExpressionAttributeNames: map[string]string{
-			"#name": "Name",
-		},
-	}
-
-	qres, err := client.Query(ctx, qinput)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	qbody, err := json.Marshal(qres)
-	if err != nil {
-		panic(err.Error())
-	}
-	println(string(qbody))
-	println()
-
-	return events.APIGatewayV2HTTPResponse{StatusCode: 200, Body: string(body)}, nil
 }
