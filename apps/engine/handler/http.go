22b0437148788100ce827e16ac26b09626a74e0f
vzsoares <vinicius.zenha@gmail.com>
2024-08-29 19:01:23 -0300

    feat: delete handler

--- apps/engine/handler/http.go
+++ apps/engine/handler/http.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"apps/engine/domain"
 	"apps/engine/tools"
+	"apps/engine/types"
 	"context"
 	"encoding/json"
 	"errors"
@@ -10,14 +11,14 @@ import (
 )
 
 type UrlHttpHandler struct {
-	store *domain.UrlDomain
-	ctx   context.Context
+	domain *domain.UrlDomain
+	ctx    context.Context
 }
 
 func NewHttpHandler(ctx context.Context, s *domain.UrlDomain) *UrlHttpHandler {
 	return &UrlHttpHandler{
-		store: s,
-		ctx:   ctx,
+		domain: s,
+		ctx:    ctx,
 	}
 }
 
@@ -32,7 +33,7 @@ func (h *UrlHttpHandler) GetHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	p, err := h.store.GetUrl(ctx, id)
+	p, err := h.domain.GetUrl(ctx, id)
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
 		if errors.Is(err, tools.ItemNotFoundError) {
@@ -48,4 +49,46 @@ func (h *UrlHttpHandler) GetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(tools.NewBody(p, "Ok", tools.CODE_OK))
+}
+
+func (h *UrlHttpHandler) DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.TODO()
+	id := r.PathValue("id")
+
+	if id == "" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(tools.NewBody(nil, "Missing id", tools.CODE_BAD_REQUEST))
+		return
+	}
+
+	err := h.domain.DeleteUrl(ctx, id)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(tools.NewBody(nil, "Error", tools.CODE_INTERNAL_SERVER_ERROR))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(tools.NewBody(nil, "Ok", tools.CODE_OK))
+}
+
+func (h *UrlHttpHandler) PostHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.TODO()
+
+	url := &types.UrlBase{}
+
+	err := h.domain.PutUrl(ctx, url)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(tools.NewBody(nil, "Error", tools.CODE_INTERNAL_SERVER_ERROR))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(tools.NewBody(nil, "Ok", tools.CODE_OK))
 }
