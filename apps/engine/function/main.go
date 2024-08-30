472abf757fb4b69589630b9bd3d4dd5566466211
vzsoares <vinicius.zenha@gmail.com>
2024-08-30 17:32:22 -0300

    fix: gateway path

--- apps/engine/function/main.go
+++ apps/engine/function/main.go
@@ -56,6 +56,7 @@ func init() {
 }
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	httpLambda.StripBasePath("/shortener/v1")
 	return httpLambda.ProxyWithContext(ctx, req)
 }
 
