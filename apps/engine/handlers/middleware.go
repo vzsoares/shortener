1c7e51fbb73db794410698621e24f95ce6535913
vzsoares <vinicius.zenha@gmail.com>
2024-08-31 12:51:09 -0300

    feat: enh auth

--- apps/engine/handlers/middleware.go
+++ apps/engine/handlers/middleware.go
@@ -2,9 +2,30 @@ package handlers
 
 import "net/http"
 
+var A4Key string
+
+func GetApiKey(key string) string {
+    // TODO pull key
+	return ""
+}
+
 func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		println("Authing...")
+		incomingKey := r.Header.Get("X-Api-Key")
+		if incomingKey == "" {
+			http.Error(w, "Missing X-Api-Key", http.StatusForbidden)
+			return
+		}
+
+		if A4Key == "" {
+			A4Key = GetApiKey("asd")
+		}
+
+		if incomingKey != A4Key {
+			http.Error(w, "Unauthorized", http.StatusForbidden)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
