3067d9253d8f10dfada1a1e332d545e5ad2329f8
vzsoares <vinicius.zenha@gmail.com>
2024-09-03 22:04:20 -0300

    feat: post handler

--- apps/public-api/function/main.go
+++ apps/public-api/function/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -8,6 +9,7 @@ import (
 	"time"
 
 	etools "apps/engine/tools"
+	"apps/engine/types"
 	"apps/public-api/tools"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -125,7 +127,83 @@ func init() {
 		w.Header().Set("Cache-Control", "no-store")
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	})
-	http.HandleFunc(buildPath("/url", &POST), func(w http.ResponseWriter, r *http.Request) {})
+
+	http.HandleFunc(buildPath("/url", &POST), func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Content-Type") != "application/json" {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(etools.NewBody(nil,
+				"content type not supported", etools.CODE_BAD_REQUEST),
+			)
+			return
+		}
+		type UrlMicro struct {
+			Destination string `dynamodbav:"destination" json:"destination"`
+		}
+
+		murl := &UrlMicro{}
+		err := json.NewDecoder(r.Body).Decode(murl)
+
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(etools.NewBody(nil,
+				"invalid json", etools.CODE_BAD_REQUEST),
+			)
+			return
+		}
+
+		now := time.Now().Unix()
+		thirtyDays := 30 * (time.Hour.Seconds() * 24)
+		url := &types.UrlBase{
+			Rash:        "p-sadw",
+			Destination: murl.Destination,
+			// Public url last 30 days
+			Ttl: int(now) + int(thirtyDays),
+		}
+
+		var byt bytes.Buffer
+		err = json.NewEncoder(&byt).Encode(&url)
+		if err != nil {
+			panic(err.Error())
+		}
+		request, err := http.NewRequest(POST, fmt.Sprintf("%v/engine/url", apiUrl), &byt)
+		if err != nil {
+			// TODO error page
+			panic(err.Error())
+		}
+
+		request.Header.Set("X-Api-Key", apiKeyA4)
+		request.Header.Set("Content-Type", "application/json")
+
+		response, err := client.Do(request)
+		if err != nil {
+			// TODO error page
+			panic(err.Error())
+		}
+		defer response.Body.Close()
+
+		body := &etools.Body{}
+		err = json.NewDecoder(response.Body).Decode(body)
+		if err != nil {
+			// TODO error page
+			panic(err.Error())
+		}
+
+		fmt.Printf("%+v\n", body)
+		if body.Code != "S200" {
+			// TODO error page
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(etools.NewBody(nil,
+				"Internal server errro", etools.CODE_INTERNAL_SERVER_ERROR),
+			)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(etools.NewBody(nil, "Ok", etools.CODE_OK))
+	})
 
 	httpLambda = httpadapter.New(http.DefaultServeMux)
 }
