9c8dd0a4b665985795fe886af13cd98e6bd170fc
vzsoares <vinicius.zenha@gmail.com>
2024-09-07 17:16:33 -0300

    feat: basic page

--- apps/front/main.go
+++ apps/front/main.go
@@ -1,12 +1,22 @@
 package main
 
-import "fmt"
-
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
-}
+import (
+	"html/template"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println(Hello("front"))
+	tmpl, err := template.ParseFiles("pages/home/index.go.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w, err := os.Create("dist/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
