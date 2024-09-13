c11d7c6acfd7a094c233b02c1bde4d158b830fd4
vzsoares <vinicius.zenha@gmail.com>
2024-09-13 20:34:21 -0300

    feat: start distribution infra

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -42,3 +42,46 @@ module "front-bucket" {
   source = "../../services/front-bucket"
   stage  = local.stage
 }
+
+# front cloudfront
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = module.front-bucket.id
+  }
+
+  enabled             = true
+  comment             = "Shortner distribution"
+  default_root_object = "index.html"
+
+  aliases = ["s.zenhalab.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = module.front-bucket.id
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  tags = {
+    Environment = local.stage
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
