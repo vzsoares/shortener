b306995701dfb5d8408df7a526d363c723a1e277
vzsoares <vinicius.zenha@gmail.com>
2024-09-13 20:08:56 -0300

    feat: extract bucket service

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -38,51 +38,7 @@ module "public-api-lambda" {
   lambda_iam_arn        = module.role.iam_role_arn
 }
 
-# front
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "s.zenhalab.com"
-
-  tags = {
-    Terraform = "true"
-    Stage     = local.stage
-  }
-}
-
-resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = aws_s3_bucket.bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "bucket" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.bucket,
-    aws_s3_bucket_public_access_block.bucket,
-  ]
-
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "public-read"
+module "front-bucket" {
+  source = "../../services/front-bucket"
+  stage  = local.stage
 }
