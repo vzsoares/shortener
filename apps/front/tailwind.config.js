079a82b16f63586b8fd35dc59dae321b98dc0144
vzsoares <vinicius.zenha@gmail.com>
2024-09-16 21:20:37 -0300

    feat: extract toaster

--- apps/front/tailwind.config.js
+++ apps/front/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/pages/**/*.html"],
+    content: ["./src/templates/**/*.html", "./src/pages/**/*.html"],
     theme: {
         extend: {
             colors: {
